91968aaf058eae3cfae38797b68d4519a5d584b2
Thomas Collins <tcollins@hubspot.com>
2021-10-05 22:16:46 +0100

    adapter for netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import preprocess from "svelte-preprocess";
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
+    adapter: adapter(),
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte'
   },
