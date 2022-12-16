8ecf5c47f3f4e71fb96731e19eccb894c3026b51
Tom Collins <tomascollins@gmail.com>
2022-12-16 15:42:52 +0000

    json api (#13)
    * json api
    
    * remove conf files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,6 @@
 <script>
 	import Library from '../components/Library.svelte';
 
-	// onMount(async() => {
-	//   const { hash } = window.location;
-	//   if(hash.includes('access_token')){
-	//     window.location.href = `/admin${hash}`;
-	//   }
-	// })
-
 	export let data;
 </script>
 
