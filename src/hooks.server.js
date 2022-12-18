88f1639e01b952a69d18c1888e5d0e0caea72d8e
collinstommy <tomascollins@gmail.com>
2022-12-18 21:05:25 +0000

    remove git auth

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,11 +1,9 @@
 import SvelteKitAuth from "@auth/sveltekit"
-import GitHub from '@auth/core/providers/github';
 import Discord from '@auth/core/providers/discord';
-import { GITHUB_ID, GITHUB_SECRET, DISCORD_ID, DISCORD_SECRET } from "$env/static/private"
+import { DISCORD_ID, DISCORD_SECRET } from "$env/static/private"
 
 export const handle = SvelteKitAuth({
   providers: [
-    GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
     Discord({ clientId: DISCORD_ID, clientSecret: DISCORD_SECRET }),
   ]
 });
